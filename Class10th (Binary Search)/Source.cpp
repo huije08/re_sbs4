01c9907878fcb37be10f0e097846805db8ef527e
huije08 <huijegang@gmail.com>
2025-11-20 19:45:50 +0900

    (2025.11.20) 이분탐색

---
+++ Class10th (Binary Search)/Source.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int search(int list[], int key, int size)
+{
+	int left = 0;
+	int right = size - 1;
+	while (true)
+	{
+		int pivot = (left + right) / 2;
+
+		if (list[pivot] = key)
+		{
+			cout << "key found : " << list[pivot] << endl;
+			return;
+		}
+		else if (list[pivot] > key)
+		{
+			right = pivot - 1;
+		}
+		else
+		{
+			left = pivot + 1;
+		}
+	}
+	cout << "not key found" << endl;
+}
+
+int main()
+{
+#pragma region 이분 탐색
+	// 탐색 범위를 반으로 나누어 찾는 값을 포함하는 범위를
+	// 좁혀나가는 방식으로 동작하는 알고리즘입니다.
+
+	int list[] = { 5, 6, 11, 13, 27, 55, 66, 92 };
+
+	int size = sizeof(list) / sizeof(list[0]);
+
+#pragma endregion
+
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,16 +2,55 @@
 
 using namespace std;
 
-int pivot(int list[])
+void sort(int list[], int start, int end)
 {
+	int pivot = start;
+
+	int left = start + 1;
+
+	int right = end;
+
+	while (left <= right)
+	{
+		// left가 end보다 작거나 같고 list[left]가\
+		/// list[pivot]보다 작거나 같을떄까지 반복
+		while (left <= end && list[pivot] >= list[left])
+		{
+			left++;
+		}
+
+		while (right > start && list[pivot] <= list[right])
+		{
+			right--;
+		}
+
+		if (left > right)
+		{
+			std::swap(list[pivot], list[right]);
+		}
+		else
+		{
+			std::swap(list[left], list[right]);
+		}
+	}
+	sort(list, start, right - 1);
+	sort(list, right - 1, end);
 
 }
 
 int main()
 {
-#pragma region 이분 탐색
-	// 탐색 범위를 반으로 나누어 찾는 값을 포함하는 범위를
-	// 좁혀나가는 방식으로 동작하는 알고리즘입니다.
+#pragma region 퀵정렬
+	// 기준점을 획득한 다음 기준점을 기준으로 배열을 나누고 한 쪽에는
+	// 기준점보다 작은 값들이 위치하게 한 다음 다른 한 쪽에는 기준점보다
+	// 큰 값들이 위치하도록 합니다.
+
+	int list[] = { 5, 3, 8, 4, 1, 6, 2, 7 };
+	int size = sizeof(list) / sizeof(list[0]);
+
+	sort(list, 0, size - 1);
+
+	
 
 #pragma endregion
 
