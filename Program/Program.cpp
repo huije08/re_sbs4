c5d337b6212e5c31d00a3e2693ee6109a03a7495
huije08 <huijegang@gmail.com>
2025-11-21 19:46:29 +0900

    (2025.11.21)

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,57 +2,63 @@
 
 using namespace std;
 
-void sort(int list[], int start, int end)
+void combine(int list[], int start, int middle, int end)
 {
-	int pivot = start;
+	int count = 0;
 
-	int left = start + 1;
+	int left = start;
+	int right = middle + 1;
 
-	int right = end;
+	int* container = new int[end - start + 1];
 
-	while (left <= right)
+	while (left <= middle && right <= end)
 	{
-		// left가 end보다 작거나 같고 list[left]가\
-		/// list[pivot]보다 작거나 같을떄까지 반복
-		while (left <= end && list[pivot] >= list[left])
+		if (list[left] <= list[right])
 		{
-			left++;
-		}
-
-		while (right > start && list[pivot] <= list[right])
-		{
-			right--;
-		}
-
-		if (left > right)
-		{
-			std::swap(list[pivot], list[right]);
+			container[count++] = list[left++];
 		}
 		else
 		{
-			std::swap(list[left], list[right]);
+			container[count++] = list[right++];
 		}
-	}
-	sort(list, start, right - 1);
-	sort(list, right - 1, end);
 
+
+
+	}
+}
+
+void merge_sort(int list[], int start, int end)
+{
+	if (start < end)
+	{
+		int middle = (start + end)/2;
+
+		merge_sort(list, start, middle);
+		merge_sort(list, middle+1,end );
+
+	}
 }
 
 int main()
 {
-#pragma region 퀵정렬
-	// 기준점을 획득한 다음 기준점을 기준으로 배열을 나누고 한 쪽에는
-	// 기준점보다 작은 값들이 위치하게 한 다음 다른 한 쪽에는 기준점보다
-	// 큰 값들이 위치하도록 합니다.
+#pragma region 합병정렬
+	// 하나의 리스트를 두 개의 균일한 크기로 분활하고 분활된
+	// 부분리스트를 정렬한 다음, 두 개의 정렬된 부분 리스트를
+	// 합하여 전체가 정렬된 리스트가 되게 하는 방법입니다.
 
-	int list[] = { 5, 3, 8, 4, 1, 6, 2, 7 };
+	// 1. 리스트의 길이가 1또는 0이면 이미 정렬된것으로봄
+	// 
+	// 2. 그렇지 않은경우
+	// 2-1. 정렬되지않은 리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로 나눕니다.
+	// 2-2. 각 부분 리스트를 재귀저긍로 합병 정렬을 이용하여 정렬합니다.
+	// 2-3. 두 부분 리스트를 다시 하나의 정렬된 리스트로 병합합니다.
+#pragma endregion
+
+
+	int list[] = { 2, 3, 5, 7, 4, 1, 8, 6 };
+
 	int size = sizeof(list) / sizeof(list[0]);
 
-	sort(list, 0, size - 1);
-
-	
-
-#pragma endregion
 
 
 	return 0;
